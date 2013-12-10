48d3495c30972ce0c6f9e7cca2e43c4b09013278
YU81 <yukiho0818@gmail.com>
2013-12-11 02:46:52 +0900

    bisectを用いた解法。 http://paiza.jp/poh/ec-campaign/result/52b22ab01f771341b1e72d78a14961b2 Test case 1 実行時間：0.09 秒 Test case 2 実行時間：0.15 秒 Test case 3 実行時間：1.81 秒

--- paiza-ec-campaign.py
+++ paiza-ec-campaign.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
 import sys
+import bisect
 
 input_lines = sys.stdin
 item_count = 0
@@ -11,27 +12,27 @@ thresholds = list()
 ans_price = list()
 line_count = 0
 
-# TODO : 二分法で再帰的に最適解を探査する
-# def get_neighbor_combination(price_list,threshold,list_count,start_index, result_list):
-#     # 指定INDEXの値が設定価格より大きければ、そのINDEXの半値でこれを再帰実行
-#     if price_list[start_index] > threshold:
-#         get_neighbor_combination(price_list,threshold,list_count,start_index / 2)
-#     # 指定INDEXの値が設定価格以下なら、
-#     elif price_list[start_index] < threshold:
-#         if price_list.count() == 0:
-#             result_list.append(price_list[start_index])
-#         elif result_list[0] + price_list[start_index] < threshold:
-#
-#
+def bisect_search(price_list, threshold, bigger_index, max_pair_sum):
+    '''
+    :param list price_list: 商品の価格リスト。ソート済み前提
+    :param int threshold: 商品の設定価格
+    :param int bigger_index: 商品のペアの内、価格が高い方の商品候補。
+    :param int max_pair_sum: 比較対象とするこれまでで最大の合計価格候補
+    '''
 
-def sum_price(price_pair):
-    return price_pair[0] + price_pair[1]
+    # 設定価格と高い方の商品の差額 
+    residue = threshold - price_list[bigger_index]
+    # 設定価格に最も近くなる安い商品の候補のインデックス
+    smaller_candidate_index = bisect.bisect_left(price_list, residue + 1) - 1 
+    if smaller_candidate_index < 0 or bigger_index == smaller_candidate_index:
+        return max_pair_sum
 
-def max_price_index_within_threshold(price_list, threshold):
-    for p in price_list:
-        if p >= threshold:
-            return price_list.index(p)
-    return len(price_list) - 1
+    # 条件を満たす2商品の合計価格候補
+    max_pair_sum_new_candidate = price_list[smaller_candidate_index] + price_list[bigger_index]
+    # 引数で渡ってきた最大価格候補と、ここで求めた2商品合計価格のうち大きい方を返す。
+    max_pair_sum = max_pair_sum_new_candidate if max_pair_sum - max_pair_sum_new_candidate <= 0 and max_pair_sum_new_candidate <= threshold else max_pair_sum
+
+    return max_pair_sum
 
 for line in input_lines:
     l = line.rstrip().split(" ")
@@ -47,26 +48,20 @@ for line in input_lines:
 item_prices.sort()
 
 for threshold in thresholds:
-    cnt = max_price_index_within_threshold(item_prices, threshold)
-    ans_diff = 10000000
-    ans = 10000000
-    for i in xrange(0, cnt + 1):
-        for j in xrange(0, cnt + 1):
-            if threshold < (item_prices[i] + item_prices[j]):
-                break
+    cnt = bisect.bisect_right(item_prices, threshold) - 1
+    ans = 0
 
-            if i != j and ans_diff > abs(threshold - (item_prices[i] + item_prices[j])):
-                ans = item_prices[i] + item_prices[j]
-                ans_diff = abs(threshold - (item_prices[i] + item_prices[j]))
+    for i in xrange(cnt,0,-1):
+        if ans == threshold:break
+        ans_candidate = bisect_search(item_prices, threshold, i, ans)
+        if ans_candidate <= threshold and ans_candidate != None:
+            if ans < ans_candidate:
+                ans = ans_candidate
 
-            if ans_diff == 0:
-                break
-
-    if ans == 10000000:
+    if ans == 0 or ans == None:
         ans_price.append(0)
     else:
         ans_price.append(ans)
 
 for ans in ans_price:
     print ans
-
